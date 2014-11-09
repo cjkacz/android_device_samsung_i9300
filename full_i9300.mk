05994de3fc82058a74e758ecde817ed4add81f34
JustArchi <JustArchi@JustArchi.net>
2014-11-09 18:19:18 +0100

    Tune dalvik for xhdpi 1024

--- full_i9300.mk
+++ full_i9300.mk
@@ -27,6 +27,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/i9300/i9300.mk)
 
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_i9300
 PRODUCT_DEVICE := i9300
